921ab5fc80f7ec97b40efe6daf91214a14af847d
Mykhailo Balaker <83900885+Angry3vilbot@users.noreply.github.com>
2023-03-06 19:11:18 +0000

    Add command registration, event handlers

--- index.ts
+++ index.ts
@@ -15,6 +15,19 @@ client.commands = new Collection()
 const commandPath = path.join(__dirname, 'commands')
 const commandFiles = readdirSync(commandPath).filter(file => file.endsWith('.ts'))
 
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = readdirSync(eventsPath).filter(file => file.endsWith('.ts'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 // Register the commands
 for (const file of commandFiles) {
     const filePath = path.join(commandPath, file)
@@ -26,31 +39,5 @@ for (const file of commandFiles) {
         console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)   
     }
 }
-
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`)
-})
-
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return
-    const client = interaction.client as CustomClient
-    const command = client.commands.get(interaction.commandName)
-
-    if(!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`)
-        return
-    }
-
-    try {
-        await command.execute(interaction)
-    } catch (err) {
-        console.error(err)
-        if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-    }
-})
 
 client.login(process.env.DISCORD_TOKEN)
